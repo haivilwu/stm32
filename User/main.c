be32828959cbd7840f190404db63e72842ebf6d0
haivil <haivil.wu@outlook.com>
2023-11-15 09:10:54 +0800

    LED流水灯

--- User/main.c
+++ User/main.c
@@ -5,17 +5,27 @@ int main(void)
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA,ENABLE);
 	GPIO_InitTypeDef GPIO_InitStructure;
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_All;
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
 	GPIO_Init(GPIOA,&GPIO_InitStructure);
-	//GPIO_ResetBits(GPIOA,GPIO_Pin_0);
-	//GPIO_SetBits(GPIOA,GPIO_Pin_0);
-	//GPIO_WriteBit(GPIOA,GPIO_Pin_0,Bit_RESET);
+
 	while(1)
 	{
-		GPIO_WriteBit(GPIOA,GPIO_Pin_0,Bit_RESET);
+		GPIO_Write(GPIOA,~0x0001);//低电平点亮（要按位取反，控制PA15~PA0）0000 0000 0000 0001
 		Delay_ms(500);
-		GPIO_WriteBit(GPIOA,GPIO_Pin_0,Bit_SET);
+		GPIO_Write(GPIOA,~0x0002);//低电平点亮（要按位取反，控制PA15~PA1）0000 0000 0000 0010
+		Delay_ms(500);		
+        GPIO_Write(GPIOA,~0x0004);//低电平点亮（要按位取反，控制PA15~PA2）0000 0000 0000 0100
+		Delay_ms(500);
+        GPIO_Write(GPIOA,~0x0008);//低电平点亮（要按位取反，控制PA15~PA3）0000 0000 0000 1000
+		Delay_ms(500);
+        GPIO_Write(GPIOA,~0x0010);//低电平点亮（要按位取反，控制PA15~PA4）0000 0000 0001 0000
+		Delay_ms(500);
+        GPIO_Write(GPIOA,~0x0020);//低电平点亮（要按位取反，控制PA15~PA5）0000 0000 0010 0000
+		Delay_ms(500);
+        GPIO_Write(GPIOA,~0x0040);//低电平点亮（要按位取反，控制PA15~PA6）0000 0000 0100 0000
+		Delay_ms(500);
+        GPIO_Write(GPIOA,~0x0080);//低电平点亮（要按位取反，控制PA15~PA7）0000 0000 1000 0000
 		Delay_ms(500);
 	}
 }
