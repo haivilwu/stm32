ed55498ab75d497596af66f7c781610b75e809fe
haivil <haivil.wu@outlook.com>
2023-11-15 09:08:45 +0800

    LED闪烁

--- User/main.c
+++ User/main.c
@@ -1,21 +1,21 @@
 #include "stm32f10x.h"                  // Device header
-
+#include "delay.h"
 int main(void)
 {
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC,ENABLE);
-    GPIO_InitTypeDef GPIO_InitStructure;
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_13;
-    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-    GPIO_Init(GPIOC, &GPIO_InitStructure);
-    //GPIO_SetBits(GPIOC, GPIO_Pin_13);//高电平
-    GPIO_ResetBits(GPIOC,GPIO_Pin_13);//低电平
-    while(1)
-    {
-//        RCC->APB2ENR = 0x00000010;
-//        GPIOC->CRH = 0x00300000;
-//        //GPIOC->ODR = 0x00002000;//灯灭
-//        GPIOC->ODR = 0x00000000;//灯亮
-    }
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA,ENABLE);
+	GPIO_InitTypeDef GPIO_InitStructure;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOA,&GPIO_InitStructure);
+	//GPIO_ResetBits(GPIOA,GPIO_Pin_0);
+	//GPIO_SetBits(GPIOA,GPIO_Pin_0);
+	//GPIO_WriteBit(GPIOA,GPIO_Pin_0,Bit_RESET);
+	while(1)
+	{
+		GPIO_WriteBit(GPIOA,GPIO_Pin_0,Bit_RESET);
+		Delay_ms(500);
+		GPIO_WriteBit(GPIOA,GPIO_Pin_0,Bit_SET);
+		Delay_ms(500);
+	}
 }
-
