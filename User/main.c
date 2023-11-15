2584698dc149fdf1994c5cae6c053c2cce36b98c
haivil <haivil.wu@outlook.com>
2023-11-15 09:28:20 +0800

    点亮中景园0.96寸 4pin i2c OLED屏

--- User/main.c
+++ User/main.c
@@ -4,25 +4,21 @@
 #include "Key.h"
 #include "Buzzer.h"
 #include "LightSensor.h"
+#include "OLED.h"
 
-//uint8_t KeyNum;
 
 int main(void)
 {
-	Buzzer_Init();
-	LightSensor_Init();
+	OLED_Init();
+	OLED_ShowChar(1,1,'A');
+	OLED_ShowString(1,3,"HelloWord!");
+	OLED_ShowNum(2,1,123456,6);
+	OLED_ShowSignedNum(2,8,123,4);
+	OLED_ShowHexNum(3,1,0x0A,2);
+	OLED_ShowBinNum(4,1,0x01,16);
 	while(1)
 	{
-	if (LightSensor_Get()==0)
-	{
-	Buzzer_ON();
-	//Delay_ms(500);
-	}
-	else
-	{
-	Buzzer_Off();
-	//Delay_ms(500);
-	}
+
 	}
 	
 }
