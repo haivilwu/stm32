0c1e4b39856804e8d19c2a43fa958e233d3e1c15
haivil <haivil.wu@outlook.com>
2023-11-15 09:13:44 +0800

    蜂鸣器

--- User/main.c
+++ User/main.c
@@ -1,31 +1,20 @@
 #include "stm32f10x.h"                  // Device header
 #include "delay.h"
+
 int main(void)
 {
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA,ENABLE);
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB,ENABLE);
 	GPIO_InitTypeDef GPIO_InitStructure;
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
 	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_All;
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-	GPIO_Init(GPIOA,&GPIO_InitStructure);
+	GPIO_Init(GPIOB,&GPIO_InitStructure);
 
 	while(1)
 	{
-		GPIO_Write(GPIOA,~0x0001);//低电平点亮（要按位取反，控制PA15~PA0）0000 0000 0000 0001
-		Delay_ms(500);
-		GPIO_Write(GPIOA,~0x0002);//低电平点亮（要按位取反，控制PA15~PA1）0000 0000 0000 0010
-		Delay_ms(500);		
-        GPIO_Write(GPIOA,~0x0004);//低电平点亮（要按位取反，控制PA15~PA2）0000 0000 0000 0100
-		Delay_ms(500);
-        GPIO_Write(GPIOA,~0x0008);//低电平点亮（要按位取反，控制PA15~PA3）0000 0000 0000 1000
-		Delay_ms(500);
-        GPIO_Write(GPIOA,~0x0010);//低电平点亮（要按位取反，控制PA15~PA4）0000 0000 0001 0000
-		Delay_ms(500);
-        GPIO_Write(GPIOA,~0x0020);//低电平点亮（要按位取反，控制PA15~PA5）0000 0000 0010 0000
-		Delay_ms(500);
-        GPIO_Write(GPIOA,~0x0040);//低电平点亮（要按位取反，控制PA15~PA6）0000 0000 0100 0000
-		Delay_ms(500);
-        GPIO_Write(GPIOA,~0x0080);//低电平点亮（要按位取反，控制PA15~PA7）0000 0000 1000 0000
-		Delay_ms(500);
+        GPIO_SetBits(GPIOB,GPIO_Pin_12);
+        Delay_ms(500);
+        GPIO_ResetBits(GPIOB,GPIO_Pin_12);
+        Delay_ms(500);
 	}
 }
